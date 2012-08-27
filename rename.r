e0a540940d7cd92bef1bb6cb8851a3188d1f2bef
seventm <konraddebski@gmail.com>
2012-08-27 11:51:51 +0200

    rename.r correct

--- rename.r
+++ rename.r
@@ -1,21 +1,20 @@
 rename<-function (oldname, newname, force = FALSE)
 {
-    newname <- deparse(substitute(newname))
-    oldname <- deparse(substitute(oldname))
-    if (!exists(oldname, envir = .GlobalEnv)) {
-        stop(paste("Variable", oldname, "does not exist!"))
-    }
-    if (exists(newname, envir = .GlobalEnv)) {
-        warning(paste("Variable", newname, "exist!"))
-    }
-    if (force == FALSE) {
-        ans <- readline(paste("Overwrite", newname, "variable? [Y|N]>"))
-        if(toupper(ans)=="N"){
-                return(invisible(FALSE))
-        }
-    }
-    assign(newname, get(oldname), envir = .GlobalEnv)
-    rm(list = as.character(get("oldname")), envir = .GlobalEnv)
-        invisible(TRUE)
+	newname <- deparse(substitute(newname))
+	oldname <- deparse(substitute(oldname))
+	if (!exists(oldname, envir = .GlobalEnv)) {
+		stop(paste("Variable", oldname, "does not exist!"))
+	}
+	if (exists(newname, envir = .GlobalEnv)) {
+		warning(paste("Variable", newname, "exist!"))
+		if (force == FALSE) {
+			ans <- readline(paste("Overwrite", newname, "variable? [Y|N]>"))
+			if(toupper(ans)=="N")
+				return(invisible(FALSE))
+    		}
+    	}
+	assign(newname, get(oldname), envir = .GlobalEnv)
+	rm(list = as.character(get("oldname")), envir = .GlobalEnv)
+	invisible(TRUE)
 }
 
